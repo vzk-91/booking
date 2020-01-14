e9d73a354b5ceca16931c9c388cb2d6eb87255dc
Vahe Karapetyan <vzkarapetyan@gmail.com>
2020-01-15 00:43:25 +0400

    Updates

---
+++ precache-manifest.18a9e16d4a97cb12f8be96a664b6e800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d0ce9c4708d676c76dc725de7f9b18",
+    "url": "/booking/index.html"
+  },
+  {
+    "revision": "6432fc1d2b25de7651cf",
+    "url": "/booking/static/css/main.e6c872dc.chunk.css"
+  },
+  {
+    "revision": "1bb4449b6ce49ee331aa",
+    "url": "/booking/static/js/2.17bf6ae2.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/booking/static/js/2.17bf6ae2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6432fc1d2b25de7651cf",
+    "url": "/booking/static/js/main.e69e3b44.chunk.js"
+  },
+  {
+    "revision": "b69c33a1c01c855c9f59",
+    "url": "/booking/static/js/runtime-main.632efb7c.js"
+  }
+]);
